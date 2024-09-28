1d9d0bfa96d2ff14b4611eeac2c3526526db5d8f
Envixity73 <157953306+Envixity73@users.noreply.github.com>
2024-09-28 22:14:53 +0100

    Update Install.sh33

--- Install.sh
+++ Install.sh
@@ -11,40 +11,40 @@ clear
 echo "Downloading Refind Theme to "$HOME" [DONE]"
 echo "Deleting older installed versions (If any) [NOT DONE]"
 
-sudo rm -rf /boot/efi/EFI/refind/{regular-theme,refind-theme-regular}
+sudo rm -rf /Volumes/EFI/refind/{regular-theme,refind-theme-regular}
 
 sleep 1
 clear
 echo "Downloading Refind Theme to "$HOME" [DONE]"
 echo "Deleteting older installed versions (If any) [DONE]"
-echo "Copying Theme to /boot/efi/EFI/refind/ [NOT DONE]"
+echo "Copying Theme to /Volumes/EFI/refind/ [NOT DONE]"
 
-sudo cp -r refind-theme-regular /boot/efi/EFI/refind/
+sudo cp -r refind-theme-regular /Volumes/EFI/refind/
 
 sleep 1
 clear
 echo "Downloading Refind Theme to "$HOME" [DONE]"
 echo "Deleteting older installed versions (If any) [DONE]"
-echo "Copying Theme to /boot/efi/EFI/refind/ [DONE]"
+echo "Copying Theme to /Volumes/EFI/refind/ [DONE]"
 echo "Removing unused directories [NOT DONE]"
 
-sudo rm -rf /boot/efi/EFI/refind/refind-theme-regular/{src,.git}
+sudo rm -rf /Volumes/EFI/refind/refind-theme-regular/{src,.git}
 
 sleep 1
 clear
 echo "Downloading Refind Theme to "$HOME" [DONE]"
 echo "Deleteting older installed versions (If any) [DONE]"
-echo "Copying Theme to /boot/efi/EFI/refind/ [DONE]"
+echo "Copying Theme to /Volumes/EFI/refind/ [DONE]"
 echo "Removing unused directories [DONE]"
 echo "Removing old themes from refind.conf [NOT DONE]"
 
-sudo sed --in-place=".bak" 's/^\s*include/# (disabled) include/' /boot/efi/EFI/refind/refind.conf
+sudo sed --in-place=".bak" 's/^\s*include/# (disabled) include/' /Volumes/EFI/refind/refind.conf
 
 sleep 1
 clear
 echo "Downloading Refind Theme to "$HOME" [DONE]"
 echo "Deleteting older installed versions (If any) [DONE]"
-echo "Copying Theme to /boot/efi/EFI/refind/ [DONE]"
+echo "Copying Theme to /Volumes/EFI/refind/ [DONE]"
 echo "Removing unused directories [DONE]"
 echo "Removing old themes from refind.conf [DONE]"
 echo "Updating refind.conf [NOT DONE]"
@@ -52,13 +52,13 @@ echo "Updating refind.conf [NOT DONE]"
 echo "
 # Load a theme!
 include refind-theme-regular/theme.conf
-" | sudo tee -a /boot/efi/EFI/refind/refind.conf &> /dev/null
+" | sudo tee -a /Volumes/EFI/refind/refind.conf &> /dev/null
 
 sleep 1
 clear
 echo "Downloading Refind Theme to "$HOME" [DONE]"
 echo "Deleteting older installed versions (If any) [DONE]"
-echo "Copying Theme to /boot/efi/EFI/refind/ [DONE]"
+echo "Copying Theme to /Volumes/EFI/refind/ [DONE]"
 echo "Removing unused directories [DONE]"
 echo "Removing old themes from refind.conf [DONE]"
 echo "Updating refind.conf [DONE]"
@@ -71,7 +71,7 @@ sleep 1
 clear
 echo "Downloading Refind Theme to "$HOME" [DONE]"
 echo "Deleteting older installed versions (If any) [DONE]"
-echo "Copying Theme to /boot/efi/EFI/refind/ [DONE]"
+echo "Copying Theme to /Volumes/EFI/refind/ [DONE]"
 echo "Removing unused directories [DONE]"
 echo "Removing old themes from refind.conf [DONE]"
 echo "Updating refind.conf [DONE]"
